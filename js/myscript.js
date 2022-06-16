6aa846d02f87ba71c7c2bdd1066b54e867ac0c1f
EmelyanovArtem <92636352+EmelyanovArtem@users.noreply.github.com>
2022-06-16 17:39:31 +0300

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -5,11 +5,12 @@ function consoleBG() {
                             'background-position': 'right', 
                             'background-size': '45% 100%', 
                             'background-repeat': 'no-repeat',
-                            'background-color' : 'transparent'});
+                            'background-color' : 'transparent',
+                            'box-shadow': 'none'});
         console.log(2);
     } else {
       $('.header__row--top').css({'background': 'white',
-                             'box-shadow': '0px 4px 15px rgba(0, 0, 0, 0.05);'});
+                             'box-shadow': '0px 4px 15px rgba(0, 0, 0, 0.05)'});
       console.log(1);
     }
   }
